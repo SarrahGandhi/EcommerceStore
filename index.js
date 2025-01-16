dad1971eb6c0b399a6608c5bb1edbacc98551cf0
Sarrah Gandhi <thesarrahgandhi@gmail.com>
2025-01-16 15:37:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || "8888";
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.get("/", (request, response) => {
+  response.render("index", { title: "Home" });
+
+  //   response.status(200).send("Test");
+});
+app.get("/about", (request, response) => {
+  response.render("about", { title: "About" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
